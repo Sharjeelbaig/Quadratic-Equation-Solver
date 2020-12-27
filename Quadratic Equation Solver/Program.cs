d43a26ee6373f1fe496493dc47906d9a8e76d6c7
Sharjeel Baig <dr.sharjeel.6@gmail.com>
2020-12-27 16:45:05 +0500

    Add files via upload

---
+++ Quadratic Equation Solver/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Quadratic_Equation_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quadratic Equation Solver");
+            Console.WriteLine("Input The Value of a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the Value of b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the value of c:");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            string positiveValue = "The Positive Value : ";
+            string negativeValue = "The Negative Value : ";
+            Console.WriteLine(positiveValue+((-b + Math.Sqrt((b*b)-(4*a*c)))/(2*a)));
+            Console.WriteLine(negativeValue + ((-b + Math.Sqrt((b * b) - (4 * a * c))) / (2 * a)));
+
+        }
+    }
+}
